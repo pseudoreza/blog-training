56bb7c50558741a1fbba8df0007c670b2ff41b88
Mirza Selim Reza <mirzaselim.reza@gmail.com>
2016-07-14 16:06:47 +0600

    fixed coding indentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,49 +3,49 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
 	def index
-    	@articles = Article.all
-  	end
+    @articles = Article.all
+  end
 
 	def show
-    	@article = Article.find(params[:id])
-  	end
+    @article = Article.find(params[:id])
+  end
 
 	def new
 	end
 
 	def edit
-  		@article = Article.find(params[:id])
+  	@article = Article.find(params[:id])
 	end
 
 	def create
 		@article = Article.new(article_params)
  	
-  		if @article.save
-    		redirect_to @article
-  		else
-    		render 'new'
-  		end
+		if @article.save
+  		redirect_to @article
+		else
+  		render 'new'
   	end
+  end
 
-  	def update
-  		@article = Article.find(params[:id])
+  def update
+  	@article = Article.find(params[:id])
  
-  		if @article.update(article_params)
-    		redirect_to @article
-  		else
-    		render 'edit'
-  		end
+  	if @article.update(article_params)
+    	redirect_to @article
+  	else
+    	render 'edit'
+  	end
 	end
 
 	def destroy
-  		@article = Article.find(params[:id])
-  		@article.destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
  
-  		redirect_to articles_path
+  	redirect_to articles_path
 	end
 
 	private
-  		def article_params
-    		params.require(:article).permit(:title, :text)
-  		end
+  	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
 end
