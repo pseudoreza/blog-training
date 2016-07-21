6d30bfd5776c4a8338cf0023d3d14a6efdd42c46
Mirza Selim Reza <mirzaselim.reza@gmail.com>
2016-07-21 17:12:35 +0600

    image field added in articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716105638) do
+ActiveRecord::Schema.define(version: 20160721110913) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -42,6 +43,9 @@ ActiveRecord::Schema.define(version: 20160716105638) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "auth_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
